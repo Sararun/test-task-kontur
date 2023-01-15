f2f1dc58e3967995418ab847eaff4227738e09f2
Sararun <sarosekvlad12@gmail.com>
2023-01-16 02:36:50 +0700

    Работаю над отправкой почты

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,16 +6,20 @@ use App\Http\Requests\UserPostRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller
 {
-    /**
-     * @param Request $request
-     * @return Request
-     */
-    public function store(UserPostRequest $request): UserResource
+
+    public function store(UserPostRequest $request)
     {
+        $admins = DB::table('users')->where('is_admin', '=', 'true');
+        foreach ($admins as $a) {
+            Mail::to($a)->send($request);
+        }
         $createUser = User::create($request->validated());
+
         return new UserResource($createUser);
     }
 }
