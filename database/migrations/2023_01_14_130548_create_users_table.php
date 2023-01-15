1d515eb4ddbc40d81fbbe7c919d7acc53820fe06
Sararun <sarosekvlad12@gmail.com>
2023-01-15 22:18:55 +0700

    Измененил миграцию и модель

--- database/migrations/2023_01_14_130548_create_users_table.php
+++ database/migrations/2023_01_14_130548_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('phone');
             $table->boolean('is_admin')->default(false);
+            $table->timestamps();
         });
     }
 
