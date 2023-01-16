22c620fab4cefb1745c4e65d32a35ff484a4eceb
Sararun <sarosekvlad12@gmail.com>
2023-01-16 18:40:05 +0700

    Сделал отправку всем админам из бд

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserPostRequest;
 use App\Http\Resources\UserResource;
+use App\Mail\UserData;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -14,12 +15,12 @@ class UserController extends Controller
 
     public function store(UserPostRequest $request)
     {
-        $admins = DB::table('users')->where('is_admin', '=', 'true');
-        foreach ($admins as $a) {
-            Mail::to($a)->send($request);
-        }
+        $admins = DB::table('users')->where('is_admin',true)->get();
         $createUser = User::create($request->validated());
-
+        foreach ($admins as $a) {
+            info($a->email);
+            Mail::to($a->email)->send(new UserData($createUser));
+        }
         return new UserResource($createUser);
     }
 }
